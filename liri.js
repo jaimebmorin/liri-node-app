97eeec84276ead7b906dd1cf33a28f16e344f9e3
Jaime <jaimebmorin@gmail.com>
2018-01-28 14:19:50 -0600

    trying to get twitter npm to work

--- liri.js
+++ liri.js
@@ -1,7 +1,10 @@
 require ('dotenv').config();
 var keys = require ('./keys.js');
-var spotify = require ('spotify');
-var twitter = require ('twitter');
+var Spotify = require ('spotify');
+var Twitter = require ('twitter');
+var omdb = require ('omdb');
+var inquirer = require ('inquirer');
+
 
 var Spotify = function(keys) {
     this.keys = keys;
@@ -9,10 +12,56 @@ var Spotify = function(keys) {
 
 var Twitter = function(keys) {
     this.keys = keys;
+    this.getTweets = function() {
+        console.log(this.keys);
+        this.get('favorites/list', function(error, tweets, response) {
+            if(error) throw error;
+            console.log(tweets);  // The favorites. 
+            console.log(response);  // Raw response object. 
+          });
+    }
 }
 
+
 var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var twitter = new Twitter(keys.twitter);
+
+
+inquirer.prompt([
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: ['my tweets', 'spotify this song', 'movie this', 'do what it says'],
+        name: "action"
+    }
+])
+.then(function(response) {
+    if (response.action === 'my tweets') {
+        twitter.getTweets();
+    } else if (response.action === 'spotify this song') {
+        spotifyThis();
+        console.log("You would like to spotify a song");
+    } else if (response.action === 'movie this') {
+        console.log("you would like to movie this");
+        movieThis();
+    } else {
+        console.log("you would like do what it says");
+        doWhatItSays();
+    }
+});
+
+
+var spotifyThis = function() {
+
+}
+
+var movieThis = function() {
+
+}
+
+var doWhatItSays = function() {
+
+}
 
 //console.log(spotify.keys.id);
 //console.log(client.keys.consumer_key);
